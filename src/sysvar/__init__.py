8030b1a5fc7ad445a6f2a6f165b22dd7c6d54875
Ilias Tsaklidis <itsaklid@uni-bonn.de>
2023-09-14 10:37:27 +0200

    Updated init

--- src/sysvar/__init__.py
+++ src/sysvar/__init__.py
@@ -14,3 +14,11 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+from os.path import dirname, basename, isfile, join
+import glob
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
