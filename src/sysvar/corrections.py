bf4126f08e697bb48b11d67daf84cc4dec85f19a
Ilias Tsaklidis <itsaklid@uni-bonn.de>
2023-09-29 15:25:48 +0200

    feat: #2 #4 first implemantion of default Belle II corrections

--- src/sysvar/corrections.py
+++ src/sysvar/corrections.py
@@ -1,22 +1,44 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Iterable
 
 import numpy as np
 
+from sysvar.utils import read_yaml
 
-@dataclass(frozen=True)
+
+class MissingInformationError(Exception):
+    pass
+
+
+@dataclass()
 class Correction:
 
-    values: np.ndarray
-    lower_bounds: np.ndarray
-    upper_bounds: np.ndarray
     dependant_variable: str
+    custom: bool = False
+    systematic: str = None
+    MC_production: str = None
+    corrections: Iterable = None
+    lower_bounds: Iterable = None
+    upper_bounds: Iterable = None
 
-    @property
-    def strings(self) -> List[str]:
-        return [
-            f"[ {low} - {up} ]" for low, up in zip(self.lower_bounds, self.upper_bounds)
-        ]
+    def __post_init__(self):
+        if self.custom:
+            # If this is a custom correction populate the fields from arguments
+            self.values = np.array(self.corrections)
+            self.lower_bounds = np.array(self.lower_bounds)
+            self.upper_bounds = np.array(self.upper_bounds)
+        else:
+            # If this is a normal Belle II correction load it from configs
+            try:
+                info = read_yaml(self.systematic, self.MC_production)
+            except TypeError:
+                raise MissingInformationError(
+                    "Need to specify the systematic effect and the MC production in the positional arguments"
+                )
+
+            self.values = info["correction"]
+            self.lower_bounds = info["min"]
+            self.upper_bounds = info["max"]
 
     @property
     def value_edges(self) -> np.ndarray:
@@ -31,4 +53,10 @@ class Correction:
         return [
             f"{low} <= {self.dependant_variable} < {up}"
             for low, up in zip(self.lower_bounds, self.upper_bounds)
+        ]
+
+    @property
+    def strings(self) -> List[str]:
+        return [
+            f"[ {low} - {up} ]" for low, up in zip(self.lower_bounds, self.upper_bounds)
         ]
