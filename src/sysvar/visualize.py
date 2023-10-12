29278f3d25d9fa23b76db573cf3e8934a5381dcf
Ilias Tsaklidis <itsaklid@uni-bonn.de>
2023-10-12 14:56:30 +0200

    feat: Save figure functionalities

--- src/sysvar/visualize.py
+++ src/sysvar/visualize.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from os import path, mkdir
+
 from typing import Union
 
 import numpy as np
@@ -5,6 +8,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 PALETTE = sns.color_palette("colorblind")
 
@@ -135,3 +139,76 @@ def plot_error_comparison_in_axis(ax: Axes, correction):
     plt.legend(bbox_to_anchor=(1, 0.7))
 
     return
+
+
+def save_figure(
+    fig: Figure,
+    fig_name_comps: str,
+    dir_spec: Union[str, None] = None,
+    extra_ext: Union[str, list, None] = None,
+):
+    """Helper function to save figure when it is called.
+    The figure needs to be passed as a argument.
+    The name of the figure is combined by default with png and pdf extensions and then saved
+    Extra extensions can be passed as arguments
+
+    Args:
+    fig: plt figure to save
+    fig_name_comps: components to combine into a single figure name
+    dir_spec: name specifier for the directory to be added after the date.
+              Defaults to None
+    extra_ext: extra extension, on top of pdf and png to save the figure with.
+    """
+    # Get the save dir. By default this is today's date
+    save_dir = _get_save_dir(dir_spec)
+    # First check if the dir exists already
+    check_if_dir_exists(save_dir)
+    extensions = _get_extensions(extra_ext)
+
+    # build the name of the figure
+    name = "_".join(fig_name_comps)
+
+    # Loop over the extensions, create the figname and then save the figure
+    for ext in extensions:
+
+        # Check if ext alread has dot in the beginning. If not add it
+        if ext[0] != ".":
+            ext.insert(0, ".")
+
+        figname = name + ext
+        fig.savefig(path.join(save_dir, figname), bbox_inches="tight", dpi=800)
+    print(f"Saved figures in {save_dir}")
+
+
+def _get_extensions(extra_ext):
+
+    # Define the defaule extensions
+    extensions = [".pdf", ".png"]
+    if extra_ext is not None:
+
+        if isinstance(extra_ext, str):
+            extensions.append(extra_ext)
+        elif isinstance(extra_ext, list):
+            extensions.extend(extra_ext)
+        else:
+            raise TypeError(f"Unkown type {type(extra_ext)} for extra_ext")
+
+    return extensions
+
+
+def _get_save_dir(dir_spec: Union[str, None] = None):
+
+    top_dir = "/nfs/dust/belle2/user/itsaklid/analysis/figures"
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    dir_name = today if dir_spec is None else "_".join((today, dir_spec))
+
+    return path.join(top_dir, dir_name)
+
+
+def check_if_dir_exists(path_to_dir):
+
+    if path.exists(path_to_dir):
+        pass
+    else:
+        mkdir(path_to_dir)
