493f698181682de1ac11c23ea7f82b2362894fa0
Ilias Tsaklidis <itsaklid@uni-bonn.de>
2024-03-22 10:08:34 +0100

    feat: Multiple small fixes

--- src/sysvar/eigendecomposer.py
+++ src/sysvar/eigendecomposer.py
@@ -78,11 +78,15 @@ class EigenDecomposer:
                 logging.info("Building template %s", str(fit_ctgy))
 
                 tmp_df = reco_df.query(
-                    f"{self.settings['ctgy_id_column']} == {fit_ctgy}"
+                    f"{self.settings['ctgy_id_column']} == '{fit_ctgy}'"
                 )
                 # Skip template create if the query yields an empty dataframe
                 if len(tmp_df) < 1:
-                    continue
+                    logging.warn("Skipping template %s", str(fit_ctgy))
+                    # PATCH FIX
+                    # This just creates an empty dataframe so that Template2D doesn't fail.
+                    # Maybe it's okay to keep doing this w/o a fix
+                    tmp_df = DataFrame(0, index=np.arange(1), columns=self.df.columns)
 
                 t = Template2D(
                     tmp_df,
@@ -138,7 +142,7 @@ class EigenDecomposer:
         max_differences = []
         for n_vectors in tqdm(range(total_N_vectors)):
 
-            dimension_subset = self.eigen_variations[:, : n_vectors + 1].T
+            dimension_subset = self.eigen_variations[:, :n_vectors].T
             subset_covariances = [self.var2cov(x) for x in dimension_subset]
 
             max_differences.append(
@@ -167,49 +171,14 @@ class EigenDecomposer:
 
     def _get_unrolled_variations(self):
 
-        # PATCH, FIXME, TODO, align fit ctgies and read them
         return self.eigen_variations[:, self.important_dims_indices].reshape(
-            len(self.decay_modes), 6, self.Nbins, self.N_important_dims
+            len(self.decay_modes),
+            len(self.fit_ctgies),
+            self.Nbins,
+            self.N_important_dims,
         )
 
     def _get_unrolled_nominals(self):
-        # PATCH, FIXME, TODO, align fit ctgies and read them
-        return self.nominal_hist.reshape(len(self.decay_modes), 6, self.Nbins)
-
-    def save_variations(self):
-
-        variations = self._get_unrolled_variations()
-        nominals = self._get_unrolled_nominals()
-
-        with uproot.update(self.settings["filename"]) as newfile:
-            logging.info("Updating file with uproot: %s", self.settings["filename"])
-
-            # PATCH, FIXME, TODO, align fit ctgies and read them
-            for i_rm, reco_mode in enumerate(self.decay_modes):
-                if reco_mode[0] in [[51103, 51104], [52103, 52104]]:
-                    template_ids = [3, 4, 5, 6, 7, 8]
-                elif reco_mode[0] in [[51101], [51102], [52101], [52102]]:
-                    template_ids = [1, 2, 3, 4, 7, 8]
-
-                for j_ctgy, ctgy in enumerate(template_ids):
-                    for k_var in range(self.N_important_dims):
-                        logging.info(
-                            "Computing template in region: %s for fit ctgy template: %s and variation #%s",
-                            reco_mode[1],
-                            ctgy,
-                            k_var + 1,
-                        )
-                        newfile[
-                            f"{reco_mode[1]}/{ctgy}/{self.syst_effect}_var{k_var+1}_up"
-                        ] = (
-                            nominals[i_rm, j_ctgy, :]
-                            + variations[i_rm, j_ctgy, :, k_var],
-                            np.linspace(0, 1, self.Nbins + 1),
-                        )
-                        newfile[
-                            f"{reco_mode[1]}/{ctgy}/{self.syst_effect}_var{k_var+1}_down"
-                        ] = (
-                            nominals[i_rm, j_ctgy, :]
-                            - variations[i_rm, j_ctgy, :, k_var],
-                            np.linspace(0, 1, self.Nbins + 1),
-                        )
+        return self.nominal_hist.reshape(
+            len(self.decay_modes), len(self.fit_ctgies), self.Nbins
+        )
